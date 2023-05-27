65162006fbba7390f6494c14374c9f834db91a40
ki-student <kiwonjune@naver.com>
2023-05-27 18:46:35 +0900

    Add, remove

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import { useState } from 'react';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+let todoItemId = 0;
 
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
+  const onSubmit = () => {
+        props.onSubmit(input);
+        setInput("");
+      };
+    
   return (<div>
     <TextField
       id="todo-item-input"
@@ -11,15 +19,74 @@ const TodoItemInputField = (props) => {
       variant="outlined"
       onChange={(e) => setInput(e.target.value)} value={input}
     />
-
+    <Button variant="outlined" onClick={onSubmit}>Add</Button>
   </div>);
   };
+
+  const TodoItem = (props) => {
+    const style = props.todoItem.isFinished ? { textDecoration: 'line-through' } : {};
+      return (<li>
+        <span style={style} onClick={() => props.onTodoItemClick(props.todoItem)}>{props.todoItem.todoItemContent}</span>
+        <Button variant="outlined" onClick={() => props.onRemoveClick(props.todoItem)}>Delete</Button>
+      </li>);
+    };    
   
+  const TodoItemList = (props) => {
+    const todoList = props.todoItemList.map((todoItem, index) => {
+      return <TodoItem
+      key={index}
+      todoItem={todoItem}
+      onTodoItemClick={props.onTodoItemClick}
+      onRemoveClick={props.onRemoveClick}
+    />;
+
+        });
+      
+      return (<div>
+        <ul>{todoList}</ul>
+      </div>);
+    };
+    
 
 function App() {
+  const [todoItemList, setTodoItemList] = useState([]);
+  const onSubmit = (newTodoItem) => {
+        setTodoItemList([...todoItemList, {
+          id: todoItemId++,
+          todoItemContent: newTodoItem,
+          isFinished: false,
+        }]);
+      };
+      const onTodoItemClick = (clickedTodoItem) => {
+            setTodoItemList(todoItemList.map((todoItem) => {
+              if (clickedTodoItem.id === todoItem.id) {
+                return {
+                  id: clickedTodoItem.id,
+                  todoItemContent: clickedTodoItem.todoItemContent,
+                  isFinished: !clickedTodoItem.isFinished,
+                };
+              } else {
+                return todoItem;
+              }
+            }));
+          };
+          const onRemoveClick = (removedTodoItem) => {
+                setTodoItemList(todoItemList.filter((todoItem) => {
+                  return todoItem.id !== removedTodoItem.id;
+                }));
+              };
+            
+    
   return (
     <div className="App">
-      <TodoItemInputField />
+      <TodoItemInputField onSubmit={onSubmit} />
+      <TodoItemList
+        todoItemList={todoItemList}
+        onTodoItemClick={onTodoItemClick}
+        onRemoveClick={onRemoveClick}
+
+      />
+
     </div>
   );
 }
